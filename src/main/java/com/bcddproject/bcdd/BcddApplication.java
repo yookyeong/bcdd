c418b47bcba8061488662f66c2e5d08d98d8f2d9
yookyeong <you_k0207@naver.com>
2024-09-24 21:27:59 +0900

    반찬딩동 초기세팅 완료

---
+++ src/main/java/com/bcddproject/bcdd/BcddApplication.java
@@ -0,0 +1,13 @@
+package com.bcddproject.bcdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcddproject/bcdd/BcddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcddproject.bcdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
